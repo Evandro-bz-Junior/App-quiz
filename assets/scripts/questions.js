1e8c41ae00cae56776ea3037bc2aaf9ef2495bf3
Evandro-bz-Junior <140557895+Evandro-bz-Junior@users.noreply.github.com>
2023-09-08 21:02:33 -0300

    App quiz

---
+++ assets/scripts/questions.js
@@ -0,0 +1,83 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "Qual nome completo de Elizabeth ?",
+        answer: "Elizabeth Da Silva Santos",
+        options: [
+            "Elizabeth Dantas da Silva",
+            "Elizabeth Da Silva Santos",
+            "Elizabeth Bezerra Santos",
+            "Elizabeth Bezerra Da Silva"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Qual Data de Nascimento de Evandro Junior ?",
+        answer: "10/12/1992",
+        options: [
+            "10/12/1992",
+            "19/04/1991",
+            "02/09/1993",
+            "25/08/1991"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Quando foi o casamento religioso de Evandro e Elizabeth?",
+        answer: "19/08/2017",
+        options: [
+            "10/10/2018",
+            "14/02/2016",
+            "19/07/2017",
+            "19/08/2017"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Quais Cidades do sul e sudeste Evandro e Elizabeth ja viajaram?",
+        answer: "Curitiba, São José dos Campos e Campos do Jordão",
+        options: [
+            "Manaus e Pará",
+            "Curitiba, São José dos Campos e Campos do Jordão",
+            "Porto Alegre e Curitiba",
+            "Rio de Janeiro e Minas Gerais"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Qual nome da marca de roupas de Elizabeth",
+        answer: "One Style",
+        options: [
+            "Eliza Modas",
+            "Seu Estilo",
+            "One Style",
+            "Eliza Roupas"
+        ]
+    },
+    /* {
+        numb: 6,
+        question: "Quantos filhos(a) tem Evandro e Elizabeth?",
+        answer: "1",
+        options: [
+            "0",
+            "2",
+            "3",
+            "1"
+        ]
+    }, */
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    //   {
+    //   numb: 6,
+    //   question: "Your Question is Here",
+    //   answer: "Correct answer of the question is here",
+    //   options: [
+    //     "Option 1",
+    //     "option 2",
+    //     "option 3",
+    //     "option 4"
+    //   ]
+    // },
+];
